69c0753301189d24e848736be2ab0722621bbda4
jdivittorio3 <jdivittorio3@gmail.com>
2015-06-06 15:25:07 -0500

    going through the logic together

--- Farkle/HomeViewController.m
+++ Farkle/HomeViewController.m
@@ -15,16 +15,21 @@
 @property (weak, nonatomic) IBOutlet UILabel *playerOneScore;
 @property (weak, nonatomic) IBOutlet UILabel *playerTwoScore;
 @property (weak, nonatomic) IBOutlet UILabel *roundScore;
+@property NSInteger roundScoreTotal;
+@property NSInteger score;
+@property NSInteger specialscore;
+
+
 @property int selectedCount;
-@property NSMutableArray *selectedDice;
+@property NSMutableArray *currentSelectedDice;
 @end
 
 @implementation HomeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.selectedDice = [NSMutableArray new];
-
+    self.currentSelectedDice = [NSMutableArray new];
+    self.roundScoreTotal = 0;
     for (DieLabel *die in self.dieLabels) {
         die.delegate = self;
     }
@@ -34,39 +39,99 @@
     die.backgroundColor = [UIColor blackColor];
     die.dieSelected = YES;
     self.button.enabled = YES;
+    [self.currentSelectedDice addObject:die];
+    [self calculateNormalScore];
+    [self calculateSpecialScore];
     [self allSelected];
-    [self.selectedDice addObject:die];
 }
 
--(void)calculateSpecialScore {
-    NSInteger score = 0;
+-(void)calculateNormalScore {
+    self.score = 0;
     NSInteger occurrencesOne = 0;
+    NSInteger occurrencesTwo = 0;
+    NSInteger occurrencesThree = 0;
+    NSInteger occurrencesFour = 0;
     NSInteger occurencesFive = 0;
-    for (DieLabel *dieLabel in self.selectedDice) {
+    NSInteger occurrencesSix = 0;
+    for (DieLabel *dieLabel in self.currentSelectedDice) {
         if ([dieLabel.text isEqualToString:@"1"]) {
             occurrencesOne += 1;
             if (occurrencesOne == 3) {
-                score = 1000;
+                self.score += 1000;
+            }
+        }
+        if ([dieLabel.text isEqualToString:@"2"]) {
+            occurrencesTwo += 1;
+            if (occurrencesTwo == 3) {
+                self.score += 200;
+            }
+        }
+        if ([dieLabel.text isEqualToString:@"3"]) {
+            occurrencesThree += 1;
+            if (occurrencesThree == 3) {
+                self.score += 300;
+            }
+        }
+        if ([dieLabel.text isEqualToString:@"4"]) {
+            occurrencesFour += 1;
+            if (occurrencesFour == 3) {
+                self.score += 400;
             }
         }
         if ([dieLabel.text isEqualToString:@"5"]) {
             occurencesFive += 1;
             if (occurencesFive == 3) {
-                score = 500;
+                self.score += 500;
+            }
+        }
+        if ([dieLabel.text isEqualToString:@"6"]) {
+            occurrencesSix += 1;
+            if (occurrencesSix == 3) {
+                self.score += 600;
+            }
+        }
+
+    }
+
+    self.roundScore.text = [NSString stringWithFormat:@"Round Score: %li", self.roundScoreTotal + self.score + self.specialscore];
+}
+
+
+-(void)calculateSpecialScore {
+    self.specialscore = 0;
+    NSInteger occurrencesOne = 0;
+    NSInteger occurencesFive = 0;
+    for (DieLabel *dieLabel in self.currentSelectedDice) {
+        if ([dieLabel.text isEqualToString:@"1"]) {
+            occurrencesOne += 1;
+            if (occurrencesOne == 1) {
+                self.specialscore += 100;
+            }
+            if (occurrencesOne == 2) {
+                self.specialscore += 200;
+            }
+        }
+        if ([dieLabel.text isEqualToString:@"5"]) {
+            occurencesFive += 1;
+            if (occurencesFive == 1) {
+                self.specialscore += 50;
+            }
+            if (occurencesFive == 2) {
+                self.specialscore += 100;
             }
         }
     }
-
-    self.roundScore.text = [NSString stringWithFormat:@"Round Score: %li", score];
+    self.roundScore.text = [NSString stringWithFormat:@"Round Score: %li", self.roundScoreTotal + self.score + self.specialscore];
 }
 
 - (IBAction)onRollButtonPressed:(UIButton *)sender {
+    self.roundScoreTotal += self.score;
     for (DieLabel *label in self.dieLabels) {
         if (!label.dieSelected) {
-            [label rollDie];
+//            [label rollDie];
         }
     }
-    [self calculateSpecialScore];
+    self.currentSelectedDice = [NSMutableArray new];
     self.button.enabled = NO;
 }
 
@@ -80,9 +145,11 @@
             for (DieLabel *label in self.dieLabels) {
                 label.dieSelected = NO;
                 label.backgroundColor = [UIColor redColor];
-                [label rollDie];
+//                [label rollDie];
             }
             self.button.enabled = YES;
+            self.roundScoreTotal = 0;
+            self.roundScore.text = @"Round Score: 0";
         }
     }
 }
