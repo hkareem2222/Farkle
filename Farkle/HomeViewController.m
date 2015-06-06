684b57efc08bedf48d33b587049b1a1148d91ffa
Husein Kareem <huseinkareem@gmail.com>
2015-06-06 12:34:20 -0500

    initial commit

---
+++ Farkle/HomeViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Farkle
+//
+//  Created by Husein Kareem on 6/6/15.
+//  Copyright (c) 2015 Husein Kareem. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+@end
